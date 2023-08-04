3cfc6d656714441fc04664a8445e40d76794baf2
Zhuoyuan <75076820+ZLI-afk@users.noreply.github.com>
2023-08-04 22:09:10 +0800

    feat(api)!: 1. support HCP structure for general Gamma line calculation 2. refactor parameter passing rule for Gamma.py

--- apex/property/Structure.py
+++ apex/property/Structure.py
@@ -4,37 +4,38 @@ from dflow.python import upload_packages
 
 upload_packages.append(__file__)
 
-class StructureType(object):
+class StructureInfo(object):
     """Analyze structure type
     Arg:
         structure: pymatgen.core.Structure object
     """
     def __init__(self, structure: pymatgen.core.Structure):
         analyzer = SpacegroupAnalyzer(structure)
-        self.space_group_symbol = analyzer.get_space_group_symbol()
-        self.space_group_number = analyzer.get_space_group_number()
-        self.point_group_symbol = analyzer.get_point_group_symbol()
-        self.crystal_system = analyzer.get_crystal_system()
-        self.lattice_type = analyzer.get_lattice_type()
-        self.num_atoms = structure.num_sites
+        self.__space_group_symbol = analyzer.get_space_group_symbol()
+        self.__space_group_number = analyzer.get_space_group_number()
+        self.__point_group_symbol = analyzer.get_point_group_symbol()
+        self.__crystal_system = analyzer.get_crystal_system()
+        self.__lattice_type = analyzer.get_lattice_type()
+        self.__num_atoms = structure.num_sites
+        self.__crystal_structure = self.__indentify_crystal()
 
-    def get_structure_type(self) -> str:
-        if self.lattice_type == 'cubic':
-            if self.num_atoms == 1 and self.space_group_symbol == 'Pm-3m':
+    def __indentify_crystal(self) -> str:
+        if self.__lattice_type == 'cubic':
+            if self.__num_atoms == 1 and self.__space_group_symbol == 'Pm-3m':
                 structure_type = 'sc'
-            elif self.num_atoms == 2 and self.space_group_symbol == 'Im-3m':
+            elif self.__num_atoms == 2 and self.__space_group_symbol == 'Im-3m':
                 structure_type = 'bcc'
-            elif self.num_atoms == 4 and self.space_group_symbol == 'Fm-3m':
+            elif self.__num_atoms == 4 and self.__space_group_symbol == 'Fm-3m':
                 structure_type = 'fcc'
-            elif self.num_atoms == 8 and self.space_group_symbol == 'Fd-3m':
+            elif self.__num_atoms == 8 and self.__space_group_symbol == 'Fd-3m':
                 structure_type = 'diamond'
             else:
                 structure_type = 'other'
 
-        elif self.lattice_type == 'hexagonal':
-            if self.num_atoms == 2 and self.space_group_symbol == 'P6_3/mmc':
+        elif self.__lattice_type == 'hexagonal':
+            if self.__num_atoms == 2 and self.__space_group_symbol == 'P6_3/mmc':
                 structure_type = 'hcp'
-            elif self.space_group_symbol == 'P6/mmm':
+            elif self.__space_group_symbol == 'P6/mmm':
                 structure_type = 'c32'
             else:
                 structure_type = 'other'
@@ -42,3 +43,31 @@ class StructureType(object):
             structure_type = 'other'
 
         return structure_type
+
+    @property
+    def space_group_symbol(self):
+        return self.__space_group_symbol
+
+    @property
+    def space_group_number(self):
+        return self.__space_group_number
+
+    @property
+    def point_group_symbol(self):
+        return self.__point_group_symbol
+
+    @property
+    def crystal_system(self):
+        return self.__crystal_system
+
+    @property
+    def lattice_type(self):
+        return self.__lattice_type
+
+    @property
+    def num_atoms(self):
+        return self.__num_atoms
+
+    @property
+    def lattice_structure(self):
+        return self.__crystal_structure
