2d68bb4895bf3f7472e9c5c3d9b5ded33fc526d6
Zhuoyuan <75076820+ZLI-afk@users.noreply.github.com>
2024-04-03 15:32:28 +0800

    fix: fail to inject run command of phonolammps under single step run condition

--- apex/core/lib/dispatcher.py
+++ apex/core/lib/dispatcher.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from dpdispatcher import (
     Machine,
@@ -23,8 +24,8 @@ def make_submission(
     errlog,
 ):
 
-    if mdata_machine["local_root"] != "./":
-        raise RuntimeError(f"local_root must be './' in machine.json.")
+    #if mdata_machine["local_root"] != "./":
+    #    raise RuntimeError(f"local_root must be './' in dpgen's machine.json.")
 
     abs_local_root = os.path.abspath("./")
 
@@ -38,6 +39,12 @@ def make_submission(
 
     task_list = []
     for ii in run_tasks:
+        # execute injected run command
+        injected_run_command = os.path.join(work_path, ii, "run_command")
+        if os.path.isfile(injected_run_command):
+            logging.info(msg=f"execute injected run_command file in {injected_run_command}")
+            with open(injected_run_command, "r") as f:
+                command = f.read()
         task = Task(
             command=command,
             task_work_path=ii,
