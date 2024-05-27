0b68e5fcca98da807186c8939ca3459e53cfb4a2
Zhuoyuan <75076820+ZLI-afk@users.noreply.github.com>
2024-05-27 16:24:53 +0800

    change: version 1.2.2

--- apex/__init__.py
+++ apex/__init__.py
@@ -1,5 +1,5 @@
 import os
-__version__ = '1.2.1'
+__version__ = '1.2.2'
 LOCAL_PATH = os.getcwd()
 
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="apex-flow",
-    version="1.2.1",
+    version="1.2.2",
     author="Zhuoyuan Li, Tongqi Wen",
     author_email="zhuoyli@outlook.com",
     description="Alloy Properties EXplorer using simulations",
