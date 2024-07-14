b978d3e80e482021a2bd4c98b3fe15d9a1b05763
Zhuoyuan <75076820+ZLI-afk@users.noreply.github.com>
2024-07-14 19:49:02 +0800

    change version tag to 1.2.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="apex-flow",
-    version="1.2.5",
+    version="1.2.6",
     author="Zhuoyuan Li, Tongqi Wen",
     author_email="zhuoyli@outlook.com",
     description="Alloy Properties EXplorer using simulations",
