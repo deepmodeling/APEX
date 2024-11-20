711d1d8ab3d6c480bbb1c0ca65911396f61719d6
Zhuoyuan <75076820+ZLI-afk@users.noreply.github.com>
2024-11-20 11:01:49 +0800

    feat: skip property calc due to mismatch relaxed structure

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="apex-flow",
-    version="1.2.12-nightly-241039",
+    version="1.2.12",
     author="Zhuoyuan Li, Tongqi Wen",
     author_email="zhuoyli@outlook.com",
     description="Alloy Properties EXplorer using simulations",
